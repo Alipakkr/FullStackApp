b5a383ede38d112374d192efaa93cc6d103c0ff8
Alipa <alipakkr24@gmail.com>
2024-03-29 22:27:53 +0530

    chnages

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -6,6 +6,7 @@ const API = axios.create({ baseURL: 'http://localhost:5000' })
 export const sendRegisterOTP = (email) => API.post('/user/send-register-otp', { email })
 export const register = (userData) => API.post('/user/register', userData)       // {name, email, password, otp}
 
-export const login = (userData) => API.put('/user/login', userData)          // {email, password}
-export const sendForgetPasswordOTP = (email) => API.post('/user/send-forget-pass-otp', { email })
-export const changePassword = (userData) => API.put('/user/change-password', userData)
+export const login = (userData) => API.put('/user/login', userData)
+    // {email, password}
+    export const sendForgetPasswordOTP = (email) => API.post('/user/send-forget-pass-otp', { email })
+    export const changePassword = (userData) => API.put('/user/change-password', userData)

225ecf85013eff88e6d522d76d2bc187f7717751
Alipa <alipakkr24@gmail.com>
2024-03-30 21:24:49 +0530

    new update

--- server/app.js
+++ server/app.js
@@ -7,12 +7,11 @@ import userRoutes from './routes/user.js'
 
 const app = express()
 dotenv.config()
-
-
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use('/user', userRoutes)
+
 mongoose.set('strictQuery', false)
 
 

a528e56ab2f4c67663abe4c44c387cc384ba9026
Alipa <alipakkr24@gmail.com>
2024-03-30 21:25:14 +0530

    new update

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,9 @@
 import express from "express"
 const router = express.Router()
 
-import { getAllPosts } from "../controllers/posts.js"
-
 import { auth } from "../middleware/auth.js"
 
+import { getAllPosts } from "../controllers/posts.js"
 // these routes can only be accessed by authenticated users
 router.get('/get-all-posts', auth, getAllPosts)
 
